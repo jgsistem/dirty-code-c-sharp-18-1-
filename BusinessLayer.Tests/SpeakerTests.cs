fb4879e1a254bf037904540ef05a1a49e2b3f078
jgsistem <jgsistem@hotmail.com>
2018-04-30 12:46:42 -0500

    Subir

---
+++ BusinessLayer.Tests/SpeakerTests.cs
@@ -0,0 +1,141 @@
+﻿using DataAccessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Tests
+{
+    [TestClass]
+	public class SpeakerTests
+	{
+		private SqlServerCompactRepository repository = new SqlServerCompactRepository(); 
+		[TestMethod]
+		public void Register_EmptyFirstName_ThrowsArgumentNullException()
+		{			
+			var speaker = GetSpeakerThatWouldBeApproved();
+			speaker.FirstName = "";
+			
+			var exception = ExceptionAssert.Throws<ArgumentNullException>( () => speaker.Register(repository));			
+			Assert.AreEqual(exception.GetType(), typeof(ArgumentNullException));
+		}
+
+		[TestMethod]
+		public void Register_EmptyLastName_ThrowsArgumentNullException()
+		{
+			var speaker = GetSpeakerThatWouldBeApproved();
+			speaker.LastName = "";
+			
+			var exception = ExceptionAssert.Throws<ArgumentNullException>(() => speaker.Register(repository));
+			Assert.AreEqual(exception.GetType(), typeof(ArgumentNullException));
+		}
+
+		[TestMethod]
+		public void Register_EmptyEmail_ThrowsArgumentNullException()
+		{
+			var speaker = GetSpeakerThatWouldBeApproved();
+			speaker.Email = "";
+			var exception = ExceptionAssert.Throws<ArgumentNullException>(() => speaker.Register(repository));
+			Assert.AreEqual(exception.GetType(), typeof(ArgumentNullException));
+		}
+
+		[TestMethod]
+		public void Register_WorksForPrestigiousEmployerButHasRedFlags_ReturnsSpeakerId()
+		{
+			
+			var speaker = GetSpeakerWithRedFlags();
+			speaker.Employer = "Microsoft";
+			int? speakerId = speaker.Register(new SqlServerCompactRepository());
+			Assert.IsFalse(speakerId == null);
+		}
+
+		[TestMethod]
+		public void Register_HasBlogButHasRedFlags_ReturnsSpeakerId()
+		{
+			var speaker = GetSpeakerWithRedFlags();
+			int? speakerId = speaker.Register(new SqlServerCompactRepository());
+			Assert.IsFalse(speakerId == null);
+		}
+
+		[TestMethod]
+		public void Register_HasCertificationsButHasRedFlags_ReturnsSpeakerId()
+		{
+			var speaker = GetSpeakerWithRedFlags();
+			speaker.Certifications = new List<string>()
+			{
+				"cert1",
+				"cert2",
+				"cert3",
+				"cert4"
+			};			
+			int? speakerId = speaker.Register(new SqlServerCompactRepository());
+			Assert.IsFalse(speakerId == null);
+		}
+
+		[TestMethod]
+		public void Register_SingleSessionThatsOnOldTech_ThrowsNoSessionsApprovedException()
+		{			
+			var speaker = GetSpeakerThatWouldBeApproved();
+			speaker.Sessions = new List<Session>() {
+				new Session("Cobol for dummies", "Intro to Cobol")
+			};		
+			var exception = ExceptionAssert.Throws<BusinessLayer.Speaker.NoSessionsApprovedException>(() => speaker.Register(repository));
+            		
+			Assert.AreEqual(exception.GetType(), typeof(Speaker.NoSessionsApprovedException));
+		}
+
+		[TestMethod]
+		public void Register_NoSessionsPassed_ThrowsArgumentException()
+		{
+			var speaker = GetSpeakerThatWouldBeApproved();
+			speaker.Sessions = new List<Session>();
+			var exception = ExceptionAssert.Throws<ArgumentException>(() => speaker.Register(repository));
+			Assert.AreEqual(exception.GetType(), typeof(ArgumentException));
+		}
+
+		[TestMethod]
+		public void Register_DoesntAppearExceptionalAndUsingOldBrowser_ThrowsNoSessionsApprovedException()
+		{
+			var speakerThatDoesntAppearExceptional = GetSpeakerThatWouldBeApproved();
+			speakerThatDoesntAppearExceptional.HasBlog = false;
+			speakerThatDoesntAppearExceptional.Browser = new WebBrowser("IE", 6);
+			var exception = ExceptionAssert.Throws<BusinessLayer.Speaker.SpeakerDoesntMeetRequirementsException>(() => speakerThatDoesntAppearExceptional.Register(repository));
+			Assert.AreEqual(exception.GetType(), typeof(Speaker.SpeakerDoesntMeetRequirementsException));
+		}
+
+		[TestMethod]
+		public void Register_DoesntAppearExceptionalAndHasAncientEmail_ThrowsNoSessionsApprovedException()
+		{
+			var speakerThatDoesntAppearExceptional = GetSpeakerThatWouldBeApproved();
+			speakerThatDoesntAppearExceptional.HasBlog = false;
+			speakerThatDoesntAppearExceptional.Email = "name@aol.com";
+            var exception = ExceptionAssert.Throws<BusinessLayer.Speaker.SpeakerDoesntMeetRequirementsException>(() => speakerThatDoesntAppearExceptional.Register(repository));
+            Assert.AreEqual(exception.GetType(), typeof(Speaker.SpeakerDoesntMeetRequirementsException));
+		}
+
+		#region Helpers
+		private Speaker GetSpeakerThatWouldBeApproved()
+		{
+            Speaker speaker = new Speaker();
+            speaker.FirstName = "First";
+			speaker.LastName = "Last";
+			speaker.Email = "example@domain.com";
+			speaker.Employer = "Example Employer";
+			speaker.HasBlog = true;
+			speaker.Browser = new WebBrowser("test", 1);
+			speaker.Exp = 1;
+			speaker.Certifications = new System.Collections.Generic.List<string>();
+			speaker.BlogURL = "";
+            speaker.Sessions = new List<Session>() { new Session("test title", "test description") };
+           return speaker;			
+		}
+
+		private Speaker GetSpeakerWithRedFlags()
+		{
+			var speaker = GetSpeakerThatWouldBeApproved();
+			speaker.Email = "tom@aol.com";
+			speaker.Browser = new WebBrowser("IE", 6);
+			return speaker;
+		}
+		#endregion
+	}
+}
---
+++ BusinessLayer/Speaker.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+
+namespace BusinessLayer
+{
+   
+    public class Speaker
+	{
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int? Exp { get; set; }
+        public bool HasBlog { get; set; }
+        public string BlogURL { get; set; }
+        public BusinessLayer.WebBrowser Browser { get; set; }
+        public List<string> Certifications { get; set; }
+        public string Employer { get; set; }
+        public int RegistrationFee { get; set; }
+        public List<BusinessLayer.Session> Sessions { get; set; }
+
+	
+		public int? Register(IRepository repository)
+		{
+			
+			int? speakerId = null;
+			bool good = false;
+			bool appr = false;
+           
+
+			var DatosPersonal = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
+            			
+			var domains = new List<string>() { "aol.com", "hotmail.com", "prodigy.com", "CompuServe.com" };
+
+            if (!string.IsNullOrWhiteSpace(FirstName))
+			{
+                if (!string.IsNullOrWhiteSpace(LastName))
+				{
+                    if (!string.IsNullOrWhiteSpace(Email))
+					{						
+						var emps = new List<string>() { "Microsoft", "Google", "Fog Creek Software", "37Signals" };
+                        good = ((Exp > Int32.Parse(ConfigurationManager.AppSettings["Exp10"]) || HasBlog || Certifications.Count() > Int32.Parse(ConfigurationManager.AppSettings["Exp3"]) || emps.Contains(Employer)));
+						if (!good)
+						{
+							string emailDomain = Email.Split('@').Last();
+							if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
+							{
+								good = true;
+							}
+						}
+
+						if (good)
+						{
+							if (Sessions.Count() != 0)
+							{
+								foreach (var session in Sessions)
+								{
+                                    foreach (var tech in DatosPersonal)
+									{
+										if (session.Title.Contains(tech) || session.Description.Contains(tech))
+										{
+											session.Approved = false;
+											break;
+										}
+										else
+										{
+											session.Approved = true;
+											appr = true;
+										}
+									}
+								}
+							}
+							else
+							{
+								throw new ArgumentException("Can't register speaker with no sessions to present.");
+							}
+
+							if (appr)
+							{
+                                if (Exp <= Int32.Parse(ConfigurationManager.AppSettings["Exp1"]))
+								{
+                                    RegistrationFee = Int32.Parse(ConfigurationManager.AppSettings["Exp500"]);
+								}
+                                else if (Exp >= Int32.Parse(ConfigurationManager.AppSettings["Exp2"]) && Exp <= Int32.Parse(ConfigurationManager.AppSettings["Exp3"]))
+								{
+                                    RegistrationFee = Int32.Parse(ConfigurationManager.AppSettings["250"]);
+								}
+                                else if (Exp >= Int32.Parse(ConfigurationManager.AppSettings["Exp4"]) && Exp <= Int32.Parse(ConfigurationManager.AppSettings["Exp5"]))
+								{
+                                    RegistrationFee = Int32.Parse(ConfigurationManager.AppSettings["Exp500"]);
+								}
+                                else if (Exp >= Int32.Parse(ConfigurationManager.AppSettings["Exp6"]) && Exp <= Int32.Parse(ConfigurationManager.AppSettings["Exp9"]))
+								{
+                                    RegistrationFee = Int32.Parse(ConfigurationManager.AppSettings["Exp50"]);
+								}
+								else
+								{
+                                    RegistrationFee = Int32.Parse(ConfigurationManager.AppSettings["Exp0"]);
+								}
+								try
+								{
+									speakerId = repository.SaveSpeaker(this);
+								}
+								catch (Exception e)
+								{
+									
+								}
+							}
+							else
+							{
+								throw new NoSessionsApprovedException("No sessions approved.");
+							}
+						}
+						else
+						{
+							throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our abitrary and capricious standards.");
+						}
+					}
+					else
+					{
+						throw new ArgumentNullException("Email is required.");
+					}
+				}
+				else
+				{
+					throw new ArgumentNullException("Last name is required.");
+				}
+			}
+			else
+			{
+				throw new ArgumentNullException("First Name is required");
+			}
+
+			return speakerId;
+		}
+
+		#region Custom Exceptions
+		public class SpeakerDoesntMeetRequirementsException : Exception
+		{
+			public SpeakerDoesntMeetRequirementsException(string message)
+				: base(message)
+			{
+			}
+
+			public SpeakerDoesntMeetRequirementsException(string format, params object[] args)
+				: base(string.Format(format, args)) { }
+		}
+
+		public class NoSessionsApprovedException : Exception
+		{
+			public NoSessionsApprovedException(string message)
+				: base(message)
+			{
+			}
+		}
+		#endregion
+	}
+}
